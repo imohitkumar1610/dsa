e56077f1130e79fb55a79ee77285da198e3f71e2
Mohit <mohitkumar91614@gmail.com>
2022-12-10 20:42:59 +0530

    commit

---
+++ adts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+struct myArray{
+    int totalSize;
+    int usedSize;
+    int *ptr;
+};
+void createArray(struct myArray* a){
+    
+}
+int main(){
+    struct myArray marks;
+    return 0;
+}
