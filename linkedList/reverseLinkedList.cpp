d170ec70ae713c6b0e23ee0d60b7d210f7850f18
Mohit <mohitkumar91614@gmail.com>
2023-03-07 16:33:23 +0530

    local changes

---
+++ linkedList/reverseLinkedList.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//function to reverse a linked list iterative solution 
+// node* reverseLinkedList(node* head){
+//     if(head == NULL || head-> next == NULL){
+//         return head;
+//     }
+//         node* pre = NULL;
+//         node* curr = head;
+//         node* forward = curr -> next;
+//         while(curr!=NULL){
+//             forward = curr -> next;
+//             curr -> next = pre;
+//             pre = curr;
+//             curr = forward;
+//         }
+//         return pre;
+// //time complexity = O(n);
+// //space complexity = O(1);
+// }
+
+//recursive solution solution 
+
+int main(){
+
+    return 0;
+}
